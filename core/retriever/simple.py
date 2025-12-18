6a7f76cc902f75b826ed020878a1c9d1ff1efb93
KeshavDaga <keshavkumardaga22@gmail.com>
2025-12-19 04:27:01 +0530

    Retriever core class

---
+++ core/retriever/simple.py
@@ -0,0 +1,11 @@
+from core.retriever.base import Retriever
+from core.llm.base import Embedder
+
+class SimpleRetriever(Retriever):
+    def __init__(self, embedder: Embedder, vector_store):
+        self.embedder = embedder
+        self.vector_store = vector_store
+
+    def retrieve(self, query: str):
+        query_vec = self.embedder.embed([query])[0]
+        return self.vector_store.search(query_vec)
